a1fcb6d4a590f8400babaa4dfdd5900da167e47b
RaphaelOdeareduo <odeareduor@mymacewan.ca>
2025-02-19 15:20:31 -0700

    set tailwind config object

---
+++ config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                martian: ["Martian Mono", "serif"],
+                inter: ["Inter", "sans-serif"]
+            },
+            colors: {
+                dark_green: "#062630",
+                
+            }
+        }
+    }
+}
