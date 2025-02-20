a5c7c44b7cb213c45b14b597f970d648899f5af4
RaphaelOdeareduo <odeareduor@mymacewan.ca>
2025-02-19 20:06:05 -0700

    Change image order in benefits section

--- src/components/ClubBenefits.jsx
+++ src/components/ClubBenefits.jsx
@@ -1,3 +1,6 @@
+import Benefit1Img from '../assets/benefit1.png'
+import Benefit2Img from '../assets/benefit2.png'
+
 const benefits = [
     'Monthly curated tech reads selected by industry experts',
     'Virtual and in-person meetups for deep-dive discussions',
@@ -8,8 +11,7 @@ const benefits = [
 const ClubBenefits = () => {
     return (
         <section className="px-5 py-12 grid md:items-center md:grid-cols-2 md:gap-y-24 md:gap-x-12">
-            {/* TODO: Fix grid content order on larger screens */}
-            <div className="benefit1-textbox">
+            <div className="benefit1-textbox order-1 md:order-2">
                 <h2 className="text-4xl font-[Martian_Mono] font-semibold tracking-tighter mb-6 ">Read together, grow together</h2>
                 <ul>
                     {benefits.map((benefit, i) => {
@@ -19,15 +21,15 @@ const ClubBenefits = () => {
                     })}
                 </ul>
             </div>
-            <div className="benefit-imagebox rounded-lg overflow-hidden shadow-lg">
-                <img className="w-full hover:scale-110 transition-scale duration-2000" src="src/assets/benefit1.png" alt="people-sitting-down-and-reading-together" />
+            <div className="benefit-imagebox rounded-lg overflow-hidden shadow-lg order-2 md:order-1">
+                <img className="w-full hover:scale-110 transition-scale duration-2000" src={Benefit1Img} alt="people-sitting-down-and-reading-together" />
             </div>
-            <div className="benefit2-textbox">
+            <div className="benefit2-textbox order-3">
                 <h2 className="text-4xl font-[Martian_Mono] font-semibold tracking-tighter mb-6">Not your average book club</h2>
                 <p className="text-xl">Connect with a community that speaks your language - from <span className="font-bold">Python</span> to <span className="font-bold">TypeScript</span> and everything in between. Our discussions blend technical depth with practical applications.</p>
             </div>
-            <div className="benefit-imagebox rounded-lg overflow-hidden shadow-lg">
-                <img className="w-full hover:scale-110 transition-scale duration-2000" src="src/assets/benefit2.png" alt="people-having-a-conversation-about-books" />
+            <div className="benefit-imagebox rounded-lg overflow-hidden shadow-lg order-4">
+                <img className="w-full hover:scale-110 transition-scale duration-2000" src={Benefit2Img} alt="people-having-a-conversation-about-books" />
             </div>
         </section>
     )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,30 @@
+import { HiArrowNarrowDown } from 'react-icons/hi'
+import heroBg from '../assets/pattern-light-bg.svg'
+import logo from '../assets/logo.svg'
+import heroImage from '../assets/hero-img.png'
+
 const Hero = () => {
     return (
-        <div className="px-5 bg-[url(src/assets/pattern-light-bg.svg)] bg-pattern bg-cover bg-no-repeat">
+        // TODO: Fix hero background image rendering
+        <div className={`px-5 bg-[url(${heroBg})] bg-cover bg-no-repeat`}>
             <header className="py-10 header mb-6">
-                <img className="logo cursor-pointer" src="src/assets/logo.svg" alt="company-logo" />
+                <img className="logo cursor-pointer" src={logo} alt="company-logo" />
             </header>
             <section className="hero flex flex-col gap-20 pb-16 md:grid md:items-center md:grid-cols-2">
                 <div className="hero-textbox flex flex-col gap-8">
                     <h1 className="text-6xl font-[Martian_Mono] font-semibold tracking-[-0.55rem]">Join the ultimate tech book club</h1>
                     <p className="text-2xl font-normal">Turn your reading time into learning time with fellow tech enthusiasts. Get curated recommendations, join vibrant discussion, and level up your skills one chapter at a time.</p>
                     <a 
-                        className="uppercase font-[Martian_Mono] text-lg font-semibold tracking-tighter bg-[#fff5ef] border-3 rounded-md p-4 flex items-center justify-center gap-8  
-                        hover:-translate-y-[2px] transition-all duration-250 active:translate-y-[2px]" 
+                        className="uppercase font-[Martian_Mono] text-lg font-semibold tracking-tighter bg-[#fff5ef] border-3 rounded-md p-4 flex items-center justify-center gap-8 shadow-lg  
+                        hover:-translate-y-[2px] hover:shadow-xl transition-all duration-250 active:translate-y-[2px] active:opacity-70 active:shadow-none" 
                         href="#"
                     >
                         <span>Review Membership Options</span>
-                        <img src="src/assets/icon-arrow-down.svg" alt="arrow-down-icon"/>
+                        <HiArrowNarrowDown />
                     </a>
                 </div>
                 <div className="hero-imagebox rounded-md overflow-hidden">
-                    <img className="w-full hover:scale-110 transition-scale duration-2000" src="src/assets/hero-img.png" alt="people-looking-at-a-book-together" />
+                    <img className="w-full hover:scale-110 transition-scale duration-2000" src={heroImage} alt="people-looking-at-a-book-together" />
                 </div>
             </section>
         </div>
