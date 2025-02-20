b65cc1811da1eca7deb020fcaee40283eca726d1
RaphaelOdeareduo <odeareduor@mymacewan.ca>
2025-02-19 17:18:33 -0700

    fine tune hero section content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,15 @@
 const Hero = () => {
     return (
-        <div className="py-12 bg-pattern bg-no-repeat">
-            <header className="header mb-6">
+        <div className="bg-pattern bg-cover bg-no-repeat">
+            <header className="py-10 header mb-6">
                 <img className="logo cursor-pointer" src="src/assets/logo.svg" alt="company-logo" />
             </header>
-            <section className="hero flex flex-col gap-20">
+            <section className="hero flex flex-col gap-20 pb-16">
                 <div className="hero-textbox flex flex-col gap-8">
-                    <h1 className="text-6xl font-martian font-semibold">Join the ultimate tech book club</h1>
+                    <h1 className="text-6xl font-[Martian_Mono] font-semibold tracking-[-0.55rem]">Join the ultimate tech book club</h1>
                     <p className="text-2xl font-normal">Turn your reading time into learning time with fellow tech enthusiasts. Get curated recommendations, join vibrant discussion, and level up your skills one chapter at a time.</p>
                     <a 
-                        className="uppercase font-martian text-lg font-medium tracking-tighter border-3 rounded-md p-4 flex items-center justify-between" 
+                        className="uppercase font-[Martian_Mono] text-lg font-semibold tracking-tighter border-3 rounded-md p-4 flex items-center justify-center gap-8" 
                         href="#"
                     >
                         <span>Review Membership Options</span>
